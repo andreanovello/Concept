273d2d78c5fa6d36aa0a32136c40c8ee1c286055
AlePicchi <alessandro.picchi@edu.itspiemonte.it>
2020-11-12 20:02:48 +0100

    Aggiunta di image picker nel popup menu

--- app/src/main/java/com/example/concept2/LoggedIn.kt
+++ app/src/main/java/com/example/concept2/LoggedIn.kt
@@ -1,26 +1,30 @@
  package com.example.concept2
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AlertDialog
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import android.Manifest
 
-class LoggedIn : AppCompatActivity() {
-    //lateinit var menuButton: ActionBarDrawerToggle
+
+ class LoggedIn : AppCompatActivity() {
     lateinit var conceptButton: Button
 
     private var mAuth: FirebaseAuth? = null
     private lateinit var user: FirebaseUser
+
+    private var galleryPermissionCode = 1001
+    private var imagePickCode = 1000
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,49 +39,13 @@ class LoggedIn : AppCompatActivity() {
 
 
     private fun initHomepage(){
-        /*
-        val menuView = findViewById<NavigationView>(R.id.menu_view)
-        menuView.setNavigationItemSelectedListener {
-            when (it.itemId){
-                R.id.item1 -> Toast.makeText(applicationContext, "Clicked ITEM 1", Toast.LENGTH_SHORT).show()
-                R.id.item2 -> Toast.makeText(applicationContext, "Clicked ITEM 2", Toast.LENGTH_SHORT).show()
-                R.id.item3 -> Toast.makeText(applicationContext, "Clicked ITEM 3", Toast.LENGTH_SHORT).show()
-            }
-            true
-        }
-
-        val drawerLayout = findViewById<DrawerLayout>(R.id.main_view)
-
-        menuButton = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(menuButton)
-        menuButton.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        */
         activateConceptMenu()
+    }
 
-    }
-    /*
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (menuButton.onOptionsItemSelected(item)){
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-    */
 
     private fun activateConceptMenu(){
         conceptButton = findViewById<Button>(R.id.menu_button)
         conceptButton.setOnClickListener {
-            /*val popUpMenuView = LayoutInflater.from(this).inflate(R.layout.menu_pop_up, null)
-            val popUpMenuBuilder = AlertDialog.Builder(this).setView(popUpMenuView).setTitle("Menu")
-            popUpMenuBuilder.setView(popUpMenuView)
-            val popUpMenuAlert = popUpMenuBuilder.show()
-
-            val closeButton = findViewById<ImageView>(R.id.close_menu_button)
-            closeButton.setOnClickListener {
-                popUpMenuAlert.dismiss()
-            }*/
 
             val menuDialog = Dialog(this)
             menuDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -89,9 +57,66 @@ class LoggedIn : AppCompatActivity() {
             }
 
             menuDialog.show()
+            activateChangePictureListener(menuDialog)
         }
     }
 
+    private fun activateChangePictureListener(dialog: Dialog){
+        dialog.findViewById<ImageView>(R.id.profile_picture).setOnClickListener {
+            //check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, galleryPermissionCode);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
 
+    }
+
+    private fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, imagePickCode)
+    }
+
+
+
+     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+         when (requestCode){
+             galleryPermissionCode -> {
+                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                     pickImageFromGallery()
+                 }
+                 else {
+                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                 }
+             }
+         }
+     }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+
+         //DOVE SALVARE L'IMMAGINE
+         val profilePicture= findViewById<ImageView>(R.id.temp)
+
+         if (resultCode == Activity.RESULT_OK && requestCode == imagePickCode){
+            profilePicture.setImageURI(data?.data)
+         }
+     }
 
 }
